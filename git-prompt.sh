3d5f61dc49244a971d3437f1022286d6a3dc6cf8
Mikko Koivunalho <mikko.koivunalho@cxense.com>
2015-04-14 09:37:59 +0200

    git-prompt.sh improvements.

--- git-prompt.sh
+++ git-prompt.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+
 (test -r "${HOME}/other/github/git/contrib/completion/git-prompt.sh") || { git clone https://github.com/git/git "${HOME}/other/github/git"; }
-source "${HOME}/other/github/git/contrib/completion/git-prompt.sh"
+
 # shellcheck disable=SC2034
 GIT_PS1_SHOWDIRTYSTATE=1
 # shellcheck disable=SC2034
@@ -12,13 +13,16 @@ GIT_PS1_SHOWUPSTREAM="auto"
 # GIT_PS1_DESCRIBE_STYLE=default
 # GIT_PS1_SHOWCOLORHINTS=1
 # GIT_PS1_HIDE_IF_PWD_IGNORED=1
+# shellcheck disable=SC2034
+GIT_PS1_STATESEPARATOR=" "
 
+source "${HOME}/other/github/git/contrib/completion/git-prompt.sh"
 source "${HOME}/portable-configuration/git-dir-colors.sh"
+
 export PROMPT_COMMAND='RET=$?;
    ERRMSG="";
    if [[ $RET != 0 ]]; then
-      ERRMSG=" ($RET)";
+      ERRMSG="($RET)";
    fi;
-   PS1="$GREEN\u@\h $BLUE\w$(__git_ps1 " (%s)")$RED$ERRMSG \\\$${BLACK} "'
-
+   PS1="$GREEN\u@\h $BLUE\w $CYAN$(__git_ps1 "(%s)") $RED$ERRMSG \\\$${BLACK} "'
 
