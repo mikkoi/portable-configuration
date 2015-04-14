85d52b02a7ce5a22311afe636a8f8ee01c6cb8c0
Mikko Koivunalho <mikko.koivunalho@cxense.com>
2015-04-14 09:09:45 +0200

    Yet more changes. Including 'git-prompt.sh'.

---
+++ git-prompt.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+(test -r "${HOME}/other/github/git/contrib/completion/git-prompt.sh") || { git clone https://github.com/git/git "${HOME}/other/github/git"; }
+source "${HOME}/other/github/git/contrib/completion/git-prompt.sh"
+# shellcheck disable=SC2034
+GIT_PS1_SHOWDIRTYSTATE=1
+# shellcheck disable=SC2034
+GIT_PS1_SHOWSTASHSTATE=1
+# shellcheck disable=SC2034
+GIT_PS1_SHOWUNTRACKEDFILES=1
+# shellcheck disable=SC2034
+GIT_PS1_SHOWUPSTREAM="auto"
+# GIT_PS1_DESCRIBE_STYLE=default
+# GIT_PS1_SHOWCOLORHINTS=1
+# GIT_PS1_HIDE_IF_PWD_IGNORED=1
+
+source "${HOME}/portable-configuration/git-dir-colors.sh"
+export PROMPT_COMMAND='RET=$?;
+   ERRMSG="";
+   if [[ $RET != 0 ]]; then
+      ERRMSG=" ($RET)";
+   fi;
+   PS1="$GREEN\u@\h $BLUE\w$(__git_ps1 " (%s)")$RED$ERRMSG \\\$${BLACK} "'
+
+
